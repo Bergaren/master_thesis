084879b747d83e2aeeee217573bd89adb00f035d
Bergaren <nisse.odhner@gmail.com>
2021-02-22 11:10:45 +0100

    hej hej

--- dataset.py
+++ dataset.py
@@ -6,117 +6,9 @@ pd.options.mode.chained_assignment = None
 import datetime
 from PyEMD import EMD, Visualisation
 from torch.utils.data import TensorDataset, DataLoader
-from kmodes.KPrototypes import KPrototypes
+from kmodes.kprototypes import KPrototypes
+from sklearn.model_selection import train_test_split
 import holidays
-
-def create_datasets(file_path, model_name, seasonality = False, EMD = False):
-    df = pd.read_csv(file_path, encoding = "ISO-8859-1", sep=',', decimal='.', index_col='Delivery', parse_dates=['Delivery'])
-    
-    df.fillna(0, inplace=True)
-    #df.dropna(inplace=True)
-    #df.fillna(method='ffill', inplace=True)
-    df.replace(to_replace=0, method='ffill',inplace=True)
-
-    if model_name == "MLP":
-        X, Y = flat_data(df, 18, seasonality)
-    elif model_name == "RNN":
-        if EMD:
-            X, Y = seq_emd_data(df, 6)
-        else:
-            X, Y = sequential_data(df, 6, seasonality)
-
-    if EMD:
-        training_generators = []
-        validate_generators = []
-        frac = round(len(X['imf 1'])*0.9)
-
-        for key in X.keys():
-            if key == 'all':
-                dataset_train = TensorDataset(X[key][:frac], Y[:frac])
-                dataset_val = TensorDataset(X[key][frac:], Y[frac:])
-
-                training_generator = DataLoader(dataset_train, shuffle = True, batch_size = 32)
-                validate_generator = DataLoader(dataset_val, shuffle = False, batch_size = 32)
-
-                training_generators.append( training_generator )
-                validate_generators.append( validate_generator )
-            else:
-                dataset_train = TensorDataset(X[key][:frac], Y[:frac])
-                dataset_val = TensorDataset(X[key][frac:], Y[frac:])
-
-                training_generator = DataLoader(dataset_train, shuffle = True, batch_size = 32)
-                validate_generator = DataLoader(dataset_val, shuffle = False, batch_size = 32)
-
-                training_generators.append( training_generator )
-                validate_generators.append( validate_generator )
-
-        return training_generators, validate_generators
-    else:
-        frac = round(len(X)*0.9)
-        dataset_train = TensorDataset(X[:frac], Y[:frac])
-        dataset_val = TensorDataset(X[frac:], Y[frac:])
-
-        training_generator = DataLoader(dataset_train, shuffle = True, batch_size = 32)
-        validate_generator = DataLoader(dataset_val, shuffle = False, batch_size = 32)
-
-        return training_generator, validate_generator
-
-def flat_data(df, lookback, seasonality):
-    start_date = datetime.datetime(2015, 1, 2, 00, 00, 00)
-    end_date = datetime.datetime(2021, 1, 1, 00, 00, 00)
-
-    delta_day = datetime.timedelta(days=1)
-    delta_twenty_three_hours = datetime.timedelta(hours=23)
-    delta_lookback = datetime.timedelta(hours=lookback)
-    delta_one_hour = datetime.timedelta(hours=1)
-
-    se_holidays = holidays.CountryHoliday('SE')
-
-    X = []
-    Y = []
-
-    while start_date < end_date:
-        todays_data = df.loc[
-            (start_date)
-            : 
-            (start_date+delta_twenty_three_hours)
-        ]
-        Y.append(
-            todays_data['Dayahead SE3'].tolist() + 
-            todays_data['Dayahead SE4'].tolist() + 
-            todays_data['Intraday SE3'].tolist() + 
-            todays_data['Intraday SE4'].tolist()
-        )
-
-        backward_data = df.loc[
-            (start_date-delta_lookback)
-            : 
-            (start_date - delta_one_hour)
-        ]
-        
-
-        X.append(
-            backward_data['Dayahead SE3 (s)'].tolist() + 
-            backward_data['Dayahead SE4 (s)'].tolist() + 
-            backward_data['Intraday SE3 (s)'][:lookback-11].tolist() + 
-            backward_data['Intraday SE4 (s)'][:lookback-11].tolist()   
-        )
-
-        if seasonality:
-            is_holiday = 1 if start_date in se_holidays else 0
-            month = [0]*12
-            month[start_date.month-1] = 1
-            day_of_week = [0]*7
-            day_of_week[start_date.weekday()] = 1
-
-            X[-1] += ([is_holiday] + month + day_of_week )
-
-        start_date += delta_day
-
-    X = torch.Tensor(X)
-    Y = torch.Tensor(Y)
-
-    return X, Y
 
 def seq_emd_data(df, lookback):
     start_date = datetime.datetime(2015, 1, lookback+1, 00, 00, 00)
@@ -212,86 +104,27 @@ def seq_emd_data(df, lookback):
     Y = torch.FloatTensor(Y)
 
     return X, Y
-    
-def sequential_data(df, lookback, seasonality):
-    start_date = datetime.datetime(2015, 1, lookback+1, 00, 00, 00)
-    end_date = datetime.datetime(2021, 1, 1, 00, 00, 00)
-
-    delta_day = datetime.timedelta(days=1)
-    delta_lookback = datetime.timedelta(days=lookback)
-    delta_twenty_three_hours = datetime.timedelta(hours=23)
-    delta_one_hour = datetime.timedelta(hours=1)
-
-    se_holidays = holidays.CountryHoliday('SE')
-
-    X = []
-    Y = []
-
-    while start_date < end_date:
-        todays_data = df.loc[
-            (start_date)
-            : 
-            (start_date+delta_twenty_three_hours)
-        ]
-        Y.append(
-            todays_data['Dayahead SE3'].tolist() + 
-            todays_data['Dayahead SE4'].tolist() + 
-            todays_data['Intraday SE3'].tolist() + 
-            todays_data['Intraday SE4'].tolist()
-        )
-
-        backward_data = df.loc[
-                (start_date-delta_lookback)
-                : 
-                (start_date-delta_one_hour)
-            ]
-
-        
-
-        backward_data['Intraday SE3 (s)'].iloc[-11:] = 0
-        backward_data['Intraday SE4 (s)'].iloc[-11:] = 0
-
-        x = np.vstack((
-            backward_data['Dayahead SE3 (s)'],
-            backward_data['Dayahead SE4 (s)'],
-            backward_data['Intraday SE3 (s)'],
-            backward_data['Intraday SE4 (s)']
-            
-        ))
-        if seasonality:
-            is_holiday = 1 if start_date in se_holidays else 0
-            month = [0]*12
-            month[start_date.month-1] = 1
-            day_of_week = [0]*7
-            day_of_week[start_date.weekday()] = 1
-            x = np.vstack((
-                x, 
-                [is_holiday]*lookback*24,
-                np.transpose(np.array([month]*lookback*24)),
-                np.transpose(np.array([day_of_week]*lookback*24))
-            ))
-
-        X.append(x)
-        start_date += delta_day
-    
-    X = torch.transpose(torch.FloatTensor(X), 1, 2)
-    Y = torch.FloatTensor(Y)
-    
-    return X, Y
 
 class Dataset():
-    def __init__(self, price_file_path = 'data/price_data.csv', model_name="RNN", seasonality = True, lookback=2, cluster = True):
+    def __init__(self, price_file_path = 'data/price_data.csv', model_name="RNN", embedded_features = False, lookback=2, k=1):
+        print("Loading dataset...")
         self.price_file_path        = price_file_path
         self.model_name             = model_name
-        self.seasonality   = seasonality
+        self.embedded_features      = embedded_features
         self.lookback               = lookback
+        self.k                      = k
         self.price_data             = self.load_price_data()
+        self.feature_data           = self.load_feature_data()
+        self.date_list              = []
         
+        if self.k > 1:
+            self.feature_data = self.create_cluster()
+            
         if model_name == "RNN":
-            self.X, self.Y = self.sequential_data()
-        if cluster:
-            self.cluster()
-        
+            self.X, self.Y = self.sequential_data() 
+        elif model_name == "MLP":
+            self.X, self.Y = self.flat_data()
+
         self.train_set, self.validate_set = self.create_generators()
 
     def load_price_data(self):
@@ -299,6 +132,67 @@ class Dataset():
         df.fillna(0, inplace=True)
         df.replace(to_replace=0, method='ffill',inplace=True)
         return df
+    
+    def load_feature_data(self):
+        df = pd.read_csv('data/feature_data.csv', encoding = "ISO-8859-1", sep=',', decimal='.', index_col='date_time', parse_dates=['date_time'])
+        df.fillna(method='ffill', inplace=True)
+        return df
+
+    def flat_data(self):
+        start_date = datetime.datetime(2015, 1, 2, 00, 00, 00)
+        end_date = datetime.datetime(2021, 1, 1, 00, 00, 00)
+
+        delta_day = datetime.timedelta(days=1)
+        delta_twenty_three_hours = datetime.timedelta(hours=23)
+        delta_lookback = datetime.timedelta(hours=self.lookback)
+        delta_one_hour = datetime.timedelta(hours=1)
+
+        X = {}
+        Y = {}
+        for i in range(self.k):
+            X[i] = []
+            Y[i] = []
+
+        while start_date < end_date:            
+            cluster = self.feature_data.loc[start_date]['cluster'] if self.k > 1 else 0
+
+            todays_data = self.price_data.loc[start_date : start_date+delta_twenty_three_hours]
+            Y[cluster].append(
+                todays_data['Dayahead SE3'].tolist() + 
+                todays_data['Dayahead SE4'].tolist() + 
+                todays_data['Intraday SE3'].tolist() + 
+                todays_data['Intraday SE4'].tolist()
+            )
+
+            backward_data = self.price_data.loc[start_date-delta_lookback : start_date - delta_one_hour]
+            X[cluster].append(
+                backward_data['Dayahead SE3 (s)'].tolist() + 
+                backward_data['Dayahead SE4 (s)'].tolist() + 
+                backward_data['Intraday SE3 (s)'][:self.lookback-11].tolist() + 
+                backward_data['Intraday SE4 (s)'][:self.lookback-11].tolist()   
+            )
+            
+            if self.embedded_features:
+                todays_f_data = self.feature_data.loc[start_date]
+                month         = self.create_one_hot( todays_f_data['month'], 12 )
+                day_of_week   = self.create_one_hot( todays_f_data['day of week'], 7)
+                weather       = todays_f_data.iloc[3:].tolist()
+
+                f_data = [todays_f_data['holiday']] + month + day_of_week + weather
+                X[cluster][-1] += f_data
+
+            start_date += delta_day
+
+        for key in X.keys():
+            X[key] = torch.Tensor(X[key])
+            Y[key] = torch.Tensor(Y[key])
+
+        return X, Y
+    
+    def create_one_hot(self, i, size):
+        array = [0]*size
+        array[int(i)] = 1
+        return array
 
     def sequential_data(self):
         start_date = datetime.datetime(2015, 1, self.lookback+1, 00, 00, 00)
@@ -309,32 +203,24 @@ class Dataset():
         delta_twenty_three_hours = datetime.timedelta(hours=23)
         delta_one_hour = datetime.timedelta(hours=1)
 
-        se_holidays = holidays.CountryHoliday('SE')
-
-        X = []
-        Y = []
+        X = {}
+        Y = {}
+        for i in range(self.k):
+            X[i] = []
+            Y[i] = []
 
         while start_date < end_date:
-            todays_data = self.price_data.loc[
-                (start_date)
-                : 
-                (start_date+delta_twenty_three_hours)
-            ]
-            Y.append(
+            cluster = self.feature_data.loc[start_date]['cluster'] if self.k > 1 else 0
+
+            todays_data = self.price_data.loc[start_date : start_date+delta_twenty_three_hours]
+            Y[cluster].append(
                 todays_data['Dayahead SE3'].tolist() + 
                 todays_data['Dayahead SE4'].tolist() + 
                 todays_data['Intraday SE3'].tolist() + 
                 todays_data['Intraday SE4'].tolist()
             )
 
-            backward_data = self.price_data.loc[
-                    (start_date-delta_lookback)
-                    : 
-                    (start_date-delta_one_hour)
-                ]
-
-            
-
+            backward_data = self.price_data.loc[start_date-delta_lookback : start_date-delta_one_hour]
             backward_data['Intraday SE3 (s)'].iloc[-11:] = 0
             backward_data['Intraday SE4 (s)'].iloc[-11:] = 0
 
@@ -343,45 +229,54 @@ class Dataset():
                 backward_data['Dayahead SE4 (s)'],
                 backward_data['Intraday SE3 (s)'],
                 backward_data['Intraday SE4 (s)']
-                
             ))
-            if self.seasonality:
-                is_holiday = 1 if start_date in se_holidays else 0
-                month = [0]*12
-                month[start_date.month-1] = 1
-                day_of_week = [0]*7
-                day_of_week[start_date.weekday()] = 1
-                x = np.vstack((
-                    x, 
-                    [is_holiday]*self.lookback*24,
-                    np.transpose(np.array([month]*self.lookback*24)),
-                    np.transpose(np.array([day_of_week]*self.lookback*24))
-                ))
 
-            X.append(x)
+            if self.embedded_features:
+                backward_f_data = self.feature_data.loc[start_date-delta_lookback : start_date-delta_one_hour]
+
+                f_data = pd.DataFrame(np.repeat(backward_f_data.values,24,axis=0))
+                f_data.columns = backward_f_data.columns
+
+                month = np.stack(f_data['month'].apply(lambda m: self.create_one_hot(m, 12)), axis=1)
+                day_of_week = np.stack(f_data['day of week'].apply(lambda d: self.create_one_hot(d, 7)), axis=1)
+
+                x = np.vstack((
+                    x,
+                    f_data['holiday'],
+                    month,
+                    day_of_week,
+                    np.transpose(f_data.iloc[:,3:])
+                ))
+                
+            X[cluster].append(x)
+
             start_date += delta_day
         
-        X = torch.transpose(torch.FloatTensor(X), 1, 2)
-        Y = torch.FloatTensor(Y)
-        
+        for key in X.keys():
+            X[key] = torch.transpose(torch.FloatTensor(X[key]), 1, 2)
+            Y[key] = torch.FloatTensor(Y[key])
+
         return X, Y
 
-    def cluster(self):
-        feature_data = pd.read_csv('data/feature_data.csv', encoding = "ISO-8859-1", sep=',', decimal='.', index_col='date_time', parse_dates=['date_time'])
-        def create_clusters():
-            clusters = KPrototypes().fit_predict(feature_data.to_numpy(), categorical=[0, 1 ,2])
-            pass
+    def create_cluster(self):
+        clusters = KPrototypes(n_clusters = self.k, verbose=1).fit_predict(self.feature_data.to_numpy(), categorical=[0, 1 ,2])
+        feature_data['cluster'] = clusters
 
-
-    def split_into_clusters(self):
-        pass
+        return feature_data
 
     def create_generators(self):
-        frac = round(len(self.X)*0.9)
-        dataset_train = TensorDataset(self.X[:frac], self.Y[:frac])
-        dataset_val = TensorDataset(self.X[frac:], self.Y[frac:])
+        training_generators = []
+        validate_generators = []
+        
+        for k in self.X.keys():
+            X_train, X_test, y_train, y_test = train_test_split(self.X[k], self.Y[k], test_size=0.1, shuffle=True)
+            dataset_train = TensorDataset(X_train, y_train)
+            dataset_val = TensorDataset(X_test, y_test)
 
-        training_generator = DataLoader(dataset_train, shuffle = True, batch_size = 32)
-        validate_generator = DataLoader(dataset_val, shuffle = False, batch_size = 32)
+            training_generator = DataLoader(dataset_train, shuffle = True, batch_size = 32)
+            validate_generator = DataLoader(dataset_val, shuffle = False, batch_size = 32)
 
-        return training_generator, validate_generator
+            training_generators.append(training_generator)
+            validate_generators.append(validate_generator)
+
+        return training_generators, validate_generators
--- main.py
+++ main.py
@@ -13,8 +13,9 @@ from tqdm import tqdm
 import holidays
 from models import RNN, MLP, EnsembleModel
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+from matplotlib.animation import FuncAnimation
 from math import sqrt
-from dataset import create_datasets, Dataset
+from dataset import Dataset
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -87,26 +88,11 @@ def validate(model, dataset):
 
         i = 1
         for column in y_pred.keys():
-            y_pred[column].extend(output[:,:24*i].flatten().tolist())
+            y_pred[column].extend(output[:,24*(i-1):24*i].flatten().tolist())
+            y_true[column].extend(target[:,24*(i-1):24*i].flatten().tolist())
             i += 1
-
-        i = 1
-        for column in y_true.keys():
-            y_true[column].extend(target[:,:24*i].flatten().tolist())
-            i += 1
-
-    d = 30
-    plt.plot(y_pred['Dayahead SE3'][24*d:24*(d+1)+1], label='Pred Dayahead')
-    plt.plot(y_true['Dayahead SE3'][24*d:24*(d+1)+1], label='True Dayahead')
-    plt.plot(y_pred['Intraday SE3'][24*d:24*(d+1)+1], label='Pred Intraday')
-    plt.plot(y_true['Intraday SE3'][24*d:24*(d+1)+1], label='True Intraday')
-    plt.title('Predicted vs actual price')
-    plt.ylabel('€/MWh')
-    plt.xlabel('Hours')
-    plt.legend()
 
     print("All L1:        {:.2f}".format( np.mean(losses) ))
-    #print(mean_absolute_error(y_true,y_pred))
     
     d = { 
         'Market': y_pred.keys(), 
@@ -121,63 +107,92 @@ def validate(model, dataset):
     print(pd.DataFrame(data=d))
     print("STD:           {:.2f}".format( np.std(y_true['Dayahead SE3']) ))
 
-    """ plt.show() """
+
+    fig1, axs = plt.subplots(2, 1)
+    colors = sns.color_palette("tab10")
+
+    delta_pred = np.subtract(y_pred['Dayahead SE4'], y_pred['Intraday SE4'])
+    delta_true = np.subtract(y_true['Dayahead SE4'], y_true['Intraday SE4'])
+    d = 30
+    axs[0].plot(delta_pred[24*d:24*(d+1)+1], label="Predicted spread")
+    axs[0].plot(delta_true[24*d:24*(d+1)+1], label="Actual spread")
+
+    print( np.sum(np.sign(delta_pred) == np.sign(delta_true)) / len(delta_pred) )
+    d_std = np.std(delta_pred)
+    d_mean = np.mean(delta_pred)
+
+    trade = []
+    for s_pred, s_true in zip(delta_pred, delta_true):
+        if np.abs(s_pred) > (1*d_std + np.abs(d_mean)):
+            trade.append( np.sign(s_pred) == np.sign(s_true) )
+
+    print(np.sum(trade) / len(trade))
+    print(len(trade))
+    #axs[0].plot(y_pred['Dayahead SE3'][24*d:24*(d+1)+1], label='Pred Dayahead', c=colors[0])
+    #axs[0].plot(y_true['Dayahead SE3'][24*d:24*(d+1)+1], label='True Dayahead', c=colors[1])
+    #axs[1].plot(y_pred['Intraday SE3'][24*d:24*(d+1)+1], label='Pred Intraday', c=colors[2])
+    #axs[1].plot(y_true['Intraday SE3'][24*d:24*(d+1)+1], label='True Intraday', c=colors[3])
+    
+    axs[0].legend()
+    plt.show()
 
 if __name__ == "__main__":
     args = sys.argv
 
-    if len(args) != 4:
-        raise Exception( (
-            "Please specify 2 arguments.\n"
-            "Argument 1: (0) - No Seasonality   | (1) - Seasonality\n"
-            "Argument 2: (0) - MLP              | (1) - NLP\n"
-            "Argument 3: (0) - Raw Price        | (1) - EMD         | (2) - VMD\n"
-            ))
-        
-    n_features = 0
-    if int(args[1]) == 0:
-        seasonality = False
-        print("(0) - No Seasonality")
-    elif int(args[1]) == 1:
-        n_features += 20
-        seasonality = True
-        print("(1) - Seasonality")
-
-    if int(args[2]) == 0:
-        print("(0) - MLP")
-        model = MLP(input_size=50+n_features, output_size=24*4, hidden_size=64)
-        model_name = type(model).__name__
-    elif int(args[2]) == 1:
-        print("(1) - LSTM")
-        if int(args[3]) == 0:
-            print("(0) - Raw price")
-            model = RNN(input_size=4+n_features, num_layers=1, output_size=24*4, hidden_size=64)
-            model.to(device)
-        elif int(args[3]) == 1:
-            print("(1) - EMD")
-            models = []
-            for i in range(1,5):    
-                model = RNN(input_size=4, num_layers=4, output_size=24*4, hidden_size=64)
-                model.to(device)
-                models.append(model)
-            #model = RNN(input_size=4*6, num_layers=1, output_size=24*4, hidden_size=64)
-        
-        model_name = "RNN"
-    
-    Dataset = Dataset(seasonality=seasonality)
-    #train_set, validate_set = create_datasets(file_path = 'data/price_data.csv', model_name = model_name, seasonality = seasonality, EMD=False )    
-
-    if int(args[3]) == 1:
-        for i, model in enumerate(models):
-            print('Training model: {}'.format(i+1))
-            models[i] = train(model, Dataset.train_set[i], epochs=20)
-            validate(model, Dataset.validate_set[i])
-        
-        ensemble_model = EnsembleModel(models, n_features=4)
-        #model = train(model, train_set[-1], epochs=20)
-        validate(ensemble_model, Dataset.validate_set[-1])
+    if len(args) != 2:
+        args = int(input(
+            "Choice a model please."
+            "Argument:    Price:    Feature Repr:     Model:  \n"
+            "(0)        - Raw       None              MLP     \n"
+            "(1)        - Raw       None              RNN     \n"
+            "(2)        - Raw       Emb. Features     MLP     \n"
+            "(3)        - Raw       Emb. Features     RNN     \n"
+            "(4)        - Raw       Clustering        MLP     \n"
+            "(5)        - Raw       Clustering        RNN     \n"
+            "(6)        - VMD       None              MLP     \n"
+            "(7)        - VMD       None              RNN     \n"
+            "(8)        - VMD       Emb. Features     MLP     \n"
+            "(9)        - VMD       Emb. Features     RNN     \n"
+            "(10)       - VMD       Clustering        MLP     \n"
+            "(11)       - VMD       Clustering        RNN     \n"
+        ))
     else:
-        model = train(model, Dataset.train_set, epochs=20)
-        validate(model, Dataset.validate_set)
-        
-        
+        arg = int(args[1])
+
+    lookback = 24
+    input_size = lookback*2 + (24-11)*2
+    k = 1
+    models = []
+    embedded_features = False
+    
+    if arg == 0:
+        models.append(MLP(input_size=input_size, output_size=24*4, hidden_size=64))
+    elif arg == 1:
+        lookback = 2
+        models.append(RNN(input_size=4, num_layers=1, output_size=24*4, hidden_size=64))
+    
+    elif arg in range(2,4):
+        seasonal_len = 20
+        weather_len = 24
+        embedded_features = True
+        if arg == 2:
+            models.append(MLP(input_size=input_size+seasonal_len+weather_len, output_size=24*4, hidden_size=64))
+        elif arg == 3:
+            lookback = 2
+            models.append(RNN(input_size=4+seasonal_len+weather_len, num_layers=1, output_size=24*4, hidden_size=64))
+
+    elif arg in range(4, 6):
+        k = 4
+        if arg == 4:
+            for i in range(k):
+                models.append(MLP(input_size=input_size, output_size=24*4, hidden_size=64))
+        else:
+            lookback = 2
+            for i in range(k):
+                models.append(RNN(input_size=4, num_layers=1, output_size=24*4, hidden_size=64))
+
+    Dataset = Dataset(model_name=type(models[0]).__name__, lookback=lookback, k=k, embedded_features=embedded_features)
+
+    for i, model in enumerate(models):
+        train(model, Dataset.train_set[i], epochs=20)
+        validate(model, Dataset.validate_set[i])
